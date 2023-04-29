63596e2e52b5355dd00bdda0ca870d26abdd04e7
Danilevich Vladislav <v.danilevitch2013@yandex.ru>
2023-04-30 00:17:51 +0300

    Заменил IMEI телефона на Android ID

--- BlackoutNotifier/app/src/main/java/com/example/blackoutnotifier/MainActivity.java
+++ BlackoutNotifier/app/src/main/java/com/example/blackoutnotifier/MainActivity.java
@@ -1,46 +1,35 @@
 package com.example.blackoutnotifier;
 
-import static android.Manifest.permission.READ_PHONE_STATE;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import  android.telephony.TelephonyManager;
+import android.provider.Settings;
 import android.view.View;
-import androidx.core.app.ActivityCompat;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    static final int REQUEST_CODE = 101;
     ClipboardManager clipboardManager;
     ClipData clip;
-    TextView textViewIMEI;
+    TextView textViewAndroidId;
     Toast toast;
+    String androidId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
-        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-        textViewIMEI = findViewById(R.id.text_view_IMEI);
+        androidId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+        textViewAndroidId = findViewById(R.id.text_view_android_id);
         toast = Toast.makeText(getApplicationContext(), "Скопировано", Toast.LENGTH_SHORT);
-        if (ActivityCompat.checkSelfPermission(this, READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{READ_PHONE_STATE}, REQUEST_CODE);
-        }
-
-        textViewIMEI.setText(telephonyManager.getDeviceId());
-
+        textViewAndroidId.setText(androidId);
         clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-
     }
 
     public void copyClick(View view) {
-        clip = ClipData.newPlainText(textViewIMEI.getText(), textViewIMEI.getText());
+        clip = ClipData.newPlainText(textViewAndroidId.getText(), textViewAndroidId.getText());
         clipboardManager.setPrimaryClip(clip);
         toast.show();
     }
