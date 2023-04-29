8143bf4e8a4301dc232dbe892d4774dc1ee80487
Danilevich Vladislav <v.danilevitch2013@yandex.ru>
2023-04-29 23:41:25 +0300

    Изменил файл MainActivity.java

--- BlackoutNotifier/app/src/main/java/com/example/blackoutnotifier/MainActivity.java
+++ BlackoutNotifier/app/src/main/java/com/example/blackoutnotifier/MainActivity.java
@@ -1,44 +1,20 @@
 package com.example.blackoutnotifier;
 
 import static android.Manifest.permission.READ_PHONE_STATE;
-
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import  android.telephony.TelephonyManager;
-
-import android.telecom.TelecomManager;
-import android.telephony.TelephonyManager;
 import android.view.View;
-
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.blackoutnotifier.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
-    final int REQUEST_CODE = 101;
+    static final int REQUEST_CODE = 101;
     ClipboardManager clipboardManager;
     ClipData clip;
     TextView textViewIMEI;
@@ -48,13 +24,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
+
         TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
         textViewIMEI = findViewById(R.id.text_view_IMEI);
         toast = Toast.makeText(getApplicationContext(), "Скопировано", Toast.LENGTH_SHORT);
         if (ActivityCompat.checkSelfPermission(this, READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            // if permissions are not provided we are requesting for permissions.
             ActivityCompat.requestPermissions(this, new String[]{READ_PHONE_STATE}, REQUEST_CODE);
         }
 
