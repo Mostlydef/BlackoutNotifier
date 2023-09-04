f9b2136fa33ae66dcfd1c4373d4355f187f328a0
Danilevich Vladislav <v.danilevitch2013@yandex.ru>
2023-09-04 10:55:04 +0300

    Добавление кнопок

---
+++ BlackoutNotifierTelegramBot/BlackoutNotifierTelegramBot/BotHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace BlackoutNotifierTelegramBot
+{
+    public static class BotHelper
+    {
+        private const string _start = "/start";
+        private const string _buttonAndroidId = "Ввести Android ID";
+        private const string _buttonView = "Посмотреть";
+        private const string _buttonClean = "Очистить";
+
+        internal static async Task Update(ITelegramBotClient telegramBotClient, Update update, CancellationToken cancellationToken)
+        {
+            switch(update.Type)
+            {
+                case Telegram.Bot.Types.Enums.UpdateType.Message:
+                    var message = update.Message;
+                    if(!String.IsNullOrEmpty(message.Text))
+                    {
+                        switch (message.Text)
+                        {
+                            case _start:
+                                await telegramBotClient.SendTextMessageAsync(message.Chat.Id, message.Text, replyMarkup: GetButtons());
+                                break;
+                            case _buttonAndroidId:
+                                await telegramBotClient.SendTextMessageAsync(message.Chat.Id, message.Text, replyMarkup: GetButtons());
+                                break;
+                            case _buttonView:
+                                await telegramBotClient.SendTextMessageAsync(message.Chat.Id, message.Text, replyMarkup: GetButtons());
+                                break;
+                            case _buttonClean:
+                                await telegramBotClient.SendTextMessageAsync(message.Chat.Id, message.Text, replyMarkup: GetButtons());
+                                break;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static IReplyMarkup? GetButtons()
+        {
+            return new ReplyKeyboardMarkup
+            (
+                new List<List<KeyboardButton>>
+                {
+                    new List<KeyboardButton>
+                    {
+                        new KeyboardButton(_buttonAndroidId), new KeyboardButton(_buttonView)
+                    },
+                    new List<KeyboardButton>
+                    {
+                        new KeyboardButton(_buttonClean)
+                    }
+                }
+            );
+        }
+
+        internal static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BlackoutNotifierTelegramBot/BlackoutNotifierTelegramBot/Program.cs
+++ BlackoutNotifierTelegramBot/BlackoutNotifierTelegramBot/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BlackoutNotifierTelegramBot;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+var client = new TelegramBotClient("6683922317:AAEEtP7hLK_LFOUAcQW4ReKXCkoxhGaaR4s");
+client.StartReceiving(BotHelper.Update, BotHelper.Error, null);
+Console.ReadLine();
 Console.WriteLine("Hello, World!");
