a55e20ba239e5439909430db302d15a673f2b8a9
Danilevich Vladislav <v.danilevitch2013@yandex.ru>
2023-09-08 15:22:30 +0300

    Созданная коллекция сервисов

--- BlackoutNotifierTelegramBot/BlackoutNotifierTelegramBot/Program.cs
+++ BlackoutNotifierTelegramBot/BlackoutNotifierTelegramBot/Program.cs
@@ -1,9 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BlackoutNotifierTelegramBot;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
-var client = new TelegramBotClient("6683922317:AAEEtP7hLK_LFOUAcQW4ReKXCkoxhGaaR4s");
-client.StartReceiving(BotHelper.Update, BotHelper.Error, null);
+var devEnvironmentVariable = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+
+var isDevelopment = string.IsNullOrEmpty(devEnvironmentVariable) ||
+                    devEnvironmentVariable.ToLower() == "development";
+
+
+var builder = new ConfigurationBuilder();
+
+builder
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+
+if (isDevelopment)
+{
+    builder.AddUserSecrets<Program>();
+}
+
+IConfigurationRoot configuration = builder.Build();
+IServiceCollection services = new ServiceCollection();
+
+services
+    .Configure<TelegramBotDetails>(configuration.GetSection(nameof(TelegramBotDetails)))
+    .AddOptions()
+    .AddLogging()
+    .AddSingleton<ITokenProvider, TokenProvider>()
+    .BuildServiceProvider();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var provider = serviceProvider.GetService<ITokenProvider>();
+
+provider.GetToken();
+
 Console.ReadLine();
-Console.WriteLine("Hello, World!");
