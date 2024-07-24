168597111222ab5927b03a746e399a9aeaa68bdd
unknown <Danilevich Vladislav>
2024-07-24 20:32:18 +0300

    шаблон телеграм бота
    (cherry picked from commit 76c7551712d231775088ddb17763307421145f38)

---
+++ BlackoutNotifier/TelegramBot/Abstract/ReceiverServiceBase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+namespace TelegramBot.Abstract
+{
+    public abstract class ReceiverServiceBase<TUpdateHandler>:IReceiverService
+        where TUpdateHandler:IUpdateHandler
+    {
+        private readonly ITelegramBotClient _botClient;
+        private readonly TUpdateHandler _updateHandler;
+        private readonly ILogger<ReceiverServiceBase<TUpdateHandler>> _logger;
+
+        internal ReceiverServiceBase(ITelegramBotClient botClient, TUpdateHandler updateHandler, ILogger<ReceiverServiceBase<TUpdateHandler>> logger)
+        {
+            _botClient = botClient;
+            _updateHandler = updateHandler;
+            _logger = logger;
+        }
+
+        public async Task ReceiveAsync(CancellationToken cancellationToken)
+        {
+            var receiverOptions = new ReceiverOptions()
+            {
+                AllowedUpdates = Array.Empty<UpdateType>(),
+                ThrowPendingUpdates = true,
+            };
+
+            var me = await _botClient.GetMeAsync(cancellationToken);
+            _logger.LogInformation("Start receiving updates for {BotName}", me.Username ?? "My Awesome Bot");
+
+            await _botClient.ReceiveAsync( updateHandler: _updateHandler, receiverOptions: receiverOptions, cancellationToken
+                : cancellationToken);
+        }
+    }
+}
--- BlackoutNotifier/TelegramBot/Program.cs
+++ BlackoutNotifier/TelegramBot/Program.cs
@@ -1,3 +1,7 @@
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using TelegramBot.Services;
+
 namespace TelegramBot
 {
     public class Program
@@ -5,13 +9,35 @@ namespace TelegramBot
         public static void Main(string[] args)
         {
             IHost host = Host.CreateDefaultBuilder(args)
-                .ConfigureServices(services =>
+                .ConfigureServices((context, services) =>
                 {
-                    services.AddHostedService<Worker>();
+                    services.Configure<BotConfiguration>(context.Configuration.GetSection(BotConfiguration.Configuration));
+                    services.AddHttpClient("telegram_bot_client")
+                    .AddTypedClient<ITelegramBotClient>((httpClient, sp) =>
+                    {
+                        BotConfiguration? botConfig = sp.GetConfiguration<BotConfiguration>();
+                        var botToken = context.Configuration.GetSection(BotConfiguration.Configuration).Get<BotConfiguration>();
+                        TelegramBotClientOptions options = new(botConfig.TelegramBotToken);
+                        return new TelegramBotClient(options, httpClient);
+                    });
+                    services.AddScoped<UpdateHandler>();
+                    services.AddScoped<ReceiverService>();
+
+                    services.AddHostedService<PollingService>();
+
+                    services.AddLogging();
                 })
                 .Build();
 
             host.Run();
         }
+
+        public class BotConfiguration
+        {
+            public static readonly string Configuration = "Telegram";
+            public string TelegramBotToken { get; set; } = "";
+            
+        }
     }
+
 }
