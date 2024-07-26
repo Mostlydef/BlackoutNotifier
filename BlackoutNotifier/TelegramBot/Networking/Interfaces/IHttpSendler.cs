41d75e09793e8cdf8538aee84d5a4ac313ad964d
Vladislav Danilevich <Danilevich Vladislav>
2024-07-26 21:46:17 +0300

    запрос на сервер с помощью http
    (cherry picked from commit b02a27317e7ea41270834004137edcdfe7cbe093)

---
+++ BlackoutNotifier/TelegramBot/Networking/Interfaces/IHttpSendler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Networking.Interfaces
+{
+    public interface IHttpSendler
+    {
+        Task<string> SendMessagePost(string message, CancellationToken cancellationToken);
+    }
+}
