6162cb336301dd447e82507dc6e4334716fca052
Vladislav Danilevich <Danilevich Vladislav>
2024-07-27 15:00:46 +0300

    убрал лишнее, переписал участок с паттерном матчинг
    (cherry picked from commit 1bdc1ca8c655733563d27b3bff6310757545dfbf)

--- BlackoutNotifier/TelegramBot/Services/UpdateHandler.cs
+++ BlackoutNotifier/TelegramBot/Services/UpdateHandler.cs
@@ -31,33 +31,38 @@ namespace TelegramBot.Services
         }
         public async Task HandleUpdateAsync(ITelegramBotClient _, Update update, CancellationToken cancellationToken)
         {
-            var handler = update switch
+            if(update.Type == UpdateType.Message && update.Message != null)
             {
-                { Message: { } message } => BotOnMessageReceived(message, cancellationToken),
-                { EditedMessage: { } message } => BotOnMessageReceived(message, cancellationToken),
-                { CallbackQuery: { } callbackQuery } => BotOnCallbackQueryReceived(callbackQuery, cancellationToken),
-                { InlineQuery: { } inlineQuery } => BotOnInlineQueryReceived(inlineQuery, cancellationToken),
-                { ChosenInlineResult: { } chosenInlineResult } => BotOnChosenInlineResultReceived(chosenInlineResult, cancellationToken),
-                _ => UnknownUpdateHandlerAsync(update, cancellationToken)
-            };
-
-            await handler;
-
+                await BotOnMessageReceived(update.Message, cancellationToken);
+            }
+            await UnknownUpdateHandlerAsync(update, cancellationToken);
         }
 
         private async Task BotOnMessageReceived(Message message, CancellationToken cancellationToken)
         {
+            string messageText;
             _logger.LogInformation("Receive message type: {MessageType}", message.Type);
-            if (message.Text is not { } messageText)
+            if (String.IsNullOrWhiteSpace(message.Text))
                 return;
-            var action = messageText.Split(' ')[0] switch
+            else
+                messageText = message.Text;
+            Message sentMessage;
+            switch (messageText)
             {
-                "/android_id" => OnStart(_botClient, message, cancellationToken),
-                "/throw" => FailingHandler(_botClient, message, cancellationToken),
-                "/start" => OnStart(_botClient, message, cancellationToken),
-                _ => Usage(_botClient, message, _httpSendler, cancellationToken)
-            };
-            Message sentMessage = await action;
+                case "/android_id":
+                    sentMessage = await OnStart(_botClient, message, cancellationToken);
+                    break;
+                case "/throw":
+                    sentMessage = await FailingHandler(_botClient, message, cancellationToken);
+                    break;
+                case "/start":
+                    sentMessage = await OnStart(_botClient, message, cancellationToken);
+                    break;
+                default:
+                    sentMessage = await Usage(_botClient, message, _httpSendler, cancellationToken);
+                    break;
+            }
+
             _logger.LogInformation("The message was sent with id: {SentMessageId}", sentMessage.MessageId);
 
 
@@ -93,50 +98,6 @@ namespace TelegramBot.Services
                 throw new IndexOutOfRangeException();
             }
 
-        }
-
-        private async Task BotOnCallbackQueryReceived(CallbackQuery callbackQuery, CancellationToken cancellationToken)
-        {
-            _logger.LogInformation("Received inline keyboard callback from: {CallbackQueryId}", callbackQuery.Id);
-
-            await _botClient.AnswerCallbackQueryAsync(
-                callbackQueryId: callbackQuery.Id,
-                text: $"Received {callbackQuery.Data}",
-                cancellationToken: cancellationToken);
-
-            await _botClient.SendTextMessageAsync(
-                chatId: callbackQuery.Message.Chat.Id,
-                text:$"Received {callbackQuery.Data}",
-                cancellationToken:cancellationToken);
-        }
-
-        private async Task BotOnInlineQueryReceived(InlineQuery inlineQuery, CancellationToken cancellationToken)
-        {
-            _logger.LogInformation("Received inline query from: {InlineQueryFromId}", inlineQuery.From.Id);
-
-            InlineQueryResult[] results = {
-            new InlineQueryResultArticle(
-                id: "1",
-                title: "TgBots",
-                inputMessageContent: new InputTextMessageContent("hello"))
-            };
-
-            await _botClient.AnswerInlineQueryAsync(
-                inlineQueryId: inlineQuery.Id,
-                results: results,
-                cacheTime: 0,
-                isPersonal: true,
-                cancellationToken: cancellationToken);
-        }
-
-        private async Task BotOnChosenInlineResultReceived(ChosenInlineResult chosenInlineResult, CancellationToken cancellationToken)
-        {
-            _logger.LogInformation("Received inline result: {ChosenInlineResultId}", chosenInlineResult.ResultId);
-
-            await _botClient.SendTextMessageAsync(
-                chatId: chosenInlineResult.From.Id,
-                text: $"You chose result with Id: {chosenInlineResult.ResultId}",
-                cancellationToken: cancellationToken);
         }
 
         private Task UnknownUpdateHandlerAsync(Update update, CancellationToken cancellationToken)
