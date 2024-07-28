1f960511a2e316b20948cb21bae4aee6475c62b5
Danilevich Vladislav <Danilevich Vladislav>
2024-07-28 20:52:18 +0300

    враппер для ILogger

---
+++ BlackoutNotifier/TelegramBot/Wrappers/LoggerWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Wrappers
+{
+    public class LoggerWrapper<T> : ILoggerWrapper<T>
+    {
+        private readonly ILogger<T> _logger;
+
+        public LoggerWrapper(ILogger<T> logger) 
+        {
+            _logger = logger;
+        }
+        public void LogInformation(string? message, params object?[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+    }
+}
