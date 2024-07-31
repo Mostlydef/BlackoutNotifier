d77f8f229990f6b3dcebb7c2b31925331053d642
Mostlydef <Danilevich Vladislav>
2024-07-31 20:04:41 +0300

    добавил новые методы
    (cherry picked from commit fd130d929ee0c91390725008c32189fbfb7629b4)

--- BlackoutNotifier/TelegramBot/Wrappers/LoggerWrapper.cs
+++ BlackoutNotifier/TelegramBot/Wrappers/LoggerWrapper.cs
@@ -14,6 +14,12 @@ namespace TelegramBot.Wrappers
         {
             _logger = logger;
         }
+
+        public void LogError(string? message, params object?[] args)
+        {
+            _logger.LogError(message, args);
+        }
+
         public void LogInformation(string? message, params object?[] args)
         {
             _logger.LogInformation(message, args);
